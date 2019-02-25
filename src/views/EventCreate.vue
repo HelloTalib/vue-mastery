f7d574c39d36b3d1e737f5760c96aad9630e538a
TALIB4590 <abutalib4590@gmail.com>
2019-02-25 13:10:41 +0600

    real-world-vue

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Event Create</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(res => {
+        this.events = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style scoped>
+</style>
